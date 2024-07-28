d510b1eb305d07fb181ab344d756790bfcd99d40
dimasabya <dimasabdulyasir9@gmail.com>
2024-07-28 13:38:10 +0700

    uji coba page

---
+++ resources/js/Layouts/Sidebar.jsx
@@ -0,0 +1,22 @@
+export function Sidebar() {
+    return (
+        <>
+            <section className="col-span-3">
+                <div className="bg-red-500">
+                    <h2>Menu</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    );
+}
