45c3bb8ea116802aa12166ca824bc4d826446c7f
dimasabya <dimasabdulyasir9@gmail.com>
2024-07-29 22:10:52 +0700

    mengedit sidebar dan dashboard

--- resources/js/Layouts/Sidebar.jsx
+++ resources/js/Layouts/Sidebar.jsx
@@ -1,20 +1,74 @@
+import Logo from "../../assets/LogoPT.png";
+import Profile from "../../assets/Profile.png";
+
+import { Link, usePage } from "@inertiajs/react";
+
 export function Sidebar() {
     return (
         <>
             <section className="col-span-2">
-                <div className="h-screen bg-red-500">
-                    <h2>Menu</h2>
-                    <ul>
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                    </ul>
+                <div className="fixed left-0 flex flex-col w-[202px] items-center h-screen gap-6 bg-red-500">
+                    <img src={Logo} alt="" className="w-[120px] h-[120px]" />
+                    <img src={Profile} alt="" className="w-[80px] h-[80px]" />
+                    <div className="flex flex-col justify-center gap-4 items center">
+                        <Link
+                            href="/"
+                            className="px-8 border"
+                            as="button"
+                            type="button"
+                        >
+                            Dashboard
+                        </Link>
+                        <Link
+                            href="/"
+                            className="px-8 border"
+                            as="button"
+                            type="button"
+                        >
+                            Kurikulum
+                        </Link>
+                        <Link
+                            href="/"
+                            className="px-8 border"
+                            as="button"
+                            type="button"
+                        >
+                            Library
+                        </Link>
+                        <Link
+                            href="/"
+                            className="px-8 border"
+                            as="button"
+                            type="button"
+                        >
+                            Training
+                        </Link>
+                        <Link
+                            href="/"
+                            className="px-8 border"
+                            as="button"
+                            type="button"
+                        >
+                            Matrix Skill
+                        </Link>
+                        <Link
+                            href="/"
+                            className="px-8 border"
+                            as="button"
+                            type="button"
+                        >
+                            License
+                        </Link>
+                        <Link
+                            href="/"
+                            className="px-8 border"
+                            as="button"
+                            type="button"
+                        >
+                            Setting
+                        </Link>
+                    </div>
+                    <button className="px-2 border rounded-full">Exit</button>
                 </div>
             </section>
         </>
--- resources/js/Pages/DashboardPage.jsx
+++ resources/js/Pages/DashboardPage.jsx
@@ -1,5 +1,7 @@
 import { Sidebar } from "@/Layouts/Sidebar";
 import { useState } from "react";
+import Bell from "../../assets/icons/bell-solid.svg";
+import Kalender from "../../assets/icons/calendar-days-solid.svg";
 
 import {
     PieChart,
@@ -13,6 +15,9 @@ import {
     Tooltip,
     Legend,
     ResponsiveContainer,
+    LabelList,
+    LineChart,
+    Line,
 } from "recharts";
 
 export default function DashboardPage(options) {
@@ -39,8 +44,62 @@ export default function DashboardPage(options) {
         { status: "KARYAWAN", jumlah: 150, fill: "#80B436" },
         { status: "MAGANG", jumlah: 120, fill: "#3D77B5" },
         { status: "LPK", jumlah: 90, fill: "#EEC643" },
-        { status: "HARIAN", jumlah: 65, fill: "#355E3B" },
+        { status: "HARIAN", jumlah: 65, fill: "#8dc696" },
     ];
+
+    const dataUsia = [
+        { status: "<20", jumlah: 50 },
+        { status: "21-30", jumlah: 100 },
+        { status: "31-40", jumlah: 150 },
+        { status: "41-50", jumlah: 120 },
+        { status: ">51", jumlah: 80 },
+    ];
+
+    const dataPendidikan = [
+        { status: "SD", jumlah: 50 },
+        { status: "SMP", jumlah: 100 },
+        { status: "SMA", jumlah: 150 },
+        { status: "D3", jumlah: 120 },
+        { status: "S1", jumlah: 120 },
+    ];
+
+    const dataJabatan = [
+        { status: "Manager", jumlah: 2 },
+        { status: "Asst Manager", jumlah: 5 },
+        { status: "Leader", jumlah: 22 },
+        { status: "Stafff", jumlah: 15 },
+        { status: "Operator", jumlah: 150 },
+    ];
+
+    const dataDepartement = [
+        { status: "Produksi", jumlah: 50 },
+        { status: "Maintenance", jumlah: 10 },
+        { status: "SCM", jumlah: 40 },
+        { status: "Engineering", jumlah: 23 },
+        { status: "Engineering NPD", jumlah: 44 },
+        { status: "HRGA", jumlah: 33 },
+        { status: "Fin & Acct", jumlah: 3 },
+        { status: "BPS", jumlah: 5 },
+        { status: "CSM", jumlah: 2 },
+        { status: "Quality", jumlah: 44 },
+        { status: "IT", jumlah: 5 },
+    ];
+
+    const reportTrainings = [
+        { status: "January", jumlah: 12 },
+        { status: "February", jumlah: 18 },
+        { status: "March", jumlah: 25 },
+        { status: "April", jumlah: 15 },
+        { status: "May", jumlah: 22 },
+        { status: "June", jumlah: 5 },
+        { status: "July", jumlah: 28 },
+        { status: "August", jumlah: 20 },
+        { status: "September", jumlah: 24 },
+        { status: "October", jumlah: 18 },
+        { status: "November", jumlah: 22 },
+        { status: "December", jumlah: 15 },
+    ];
+
     const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#AA336A"];
 
     const renderCustomLegend = (props) => {
@@ -95,7 +154,7 @@ export default function DashboardPage(options) {
                 fill="black"
                 textAnchor="middle"
                 dominantBaseline="central"
-                fontSize="16"
+                fontSize="10"
             >
                 {value}
             </text>
@@ -107,25 +166,46 @@ export default function DashboardPage(options) {
                 <Sidebar />
                 <div
                     className={` col-span-10 
-                         bg-yellow-900 p-2
+                         bg-gray-300 p-2
                     `}
                 >
                     <nav className="w-full p-2">
                         <div className="flex justify-between">
-                            <button className="px-4 py-1 bg-indigo-500 border rounded-md">
-                                Master Karyawan
-                            </button>
-                            <div>
-                                <button className="px-4 py-1 border">
-                                    bulan
+                            <div className="flex items-center">
+                                <button className="px-4 py-1 font-semibold bg-indigo-500 border rounded-md">
+                                    Master Karyawan
                                 </button>
-                                <button className="px-4 py-1 ml-4 border">
-                                    tahun
+                                <button className="mx-2">
+                                    <img
+                                        src={Bell}
+                                        alt="bell"
+                                        className="w-8 h-8"
+                                    />
+                                </button>
+                            </div>
+                            <div>
+                                <button className="px-4 py-1 bg-gray-400 border rounded-md outline outline-[1.5px] ">
+                                    <span className="font-semibold">
+                                        Januari
+                                    </span>
+                                    <img
+                                        src={Kalender}
+                                        alt=""
+                                        className="inline-block w-5 h-5 ml-2"
+                                    />
+                                </button>
+                                <button className="px-4 py-1 ml-4 bg-gray-400 border rounded-md outline outline-[1.5px] ">
+                                    <span className="font-semibold">2024</span>
+                                    <img
+                                        src={Kalender}
+                                        alt=""
+                                        className="inline-block w-5 h-5 ml-2"
+                                    />
                                 </button>
                             </div>
                         </div>
                     </nav>
-                    <div className="flex flex-wrap items-center justify-center gap-4">
+                    <div className="flex flex-wrap items-center justify-center gap-4 my-4">
                         <div className="border p-2 w-[220px] h-[200px]">
                             <h1>Status Karyawan</h1>
                             <ResponsiveContainer width="100%" height={150}>
@@ -167,23 +247,22 @@ export default function DashboardPage(options) {
                             </ResponsiveContainer>
                         </div>
                         <div className="border p-2 w-[220px] h-[200px]">
-                            <h1>Status Karyawan</h1>
-                            <ResponsiveContainer width="100%" height={150}>
+                            <h1>Data Gender</h1>
+                            <p className="text-xs text-gray-500">
+                                24 Per Hari Ini
+                            </p>
+                            <ResponsiveContainer width="100%" height={120}>
                                 <PieChart>
                                     <Pie
-                                        data={statusData}
+                                        data={genderData}
                                         dataKey="jumlah"
-                                        nameKey="status"
+                                        nameKey="gender"
                                         cx="50%"
                                         cy="50%"
                                         outerRadius={50}
                                         fill="#8884d8"
                                         label={renderCustomLabel}
                                         labelLine={false}
-                                        // labelStyle={{
-                                        //     fontSize: "12px", // Set font size here
-                                        //     fontWeight: "bold",
-                                        // }}
                                     >
                                         {statusData.map((entry, index) => (
                                             <Cell
@@ -207,15 +286,16 @@ export default function DashboardPage(options) {
                             </ResponsiveContainer>
                         </div>
                         <div className="border p-2 w-[220px] h-[200px]">
-                            <h1>Status Karyawan</h1>
+                            <h1>Usia</h1>
                             <ResponsiveContainer width="100%" height={150}>
                                 <PieChart>
                                     <Pie
-                                        data={statusData}
+                                        data={dataUsia}
                                         dataKey="jumlah"
                                         nameKey="status"
                                         cx="50%"
                                         cy="50%"
+                                        innerRadius={30}
                                         outerRadius={50}
                                         fill="#8884d8"
                                         label={renderCustomLabel}
@@ -225,10 +305,10 @@ export default function DashboardPage(options) {
                                         //     fontWeight: "bold",
                                         // }}
                                     >
-                                        {statusData.map((entry, index) => (
+                                        {dataUsia.map((entry, index) => (
                                             <Cell
                                                 key={`cell-${index}`}
-                                                fill={entry.fill}
+                                                fill={COLORS[index]}
                                             />
                                         ))}
                                     </Pie>
@@ -247,11 +327,11 @@ export default function DashboardPage(options) {
                             </ResponsiveContainer>
                         </div>
                         <div className="border p-2 w-[220px] h-[200px]">
-                            <h1>Status Karyawan</h1>
+                            <h1>Pendidikan</h1>
                             <ResponsiveContainer width="100%" height={150}>
                                 <PieChart>
                                     <Pie
-                                        data={statusData}
+                                        data={dataPendidikan}
                                         dataKey="jumlah"
                                         nameKey="status"
                                         cx="50%"
@@ -260,10 +340,6 @@ export default function DashboardPage(options) {
                                         fill="#8884d8"
                                         label={renderCustomLabel}
                                         labelLine={false}
-                                        // labelStyle={{
-                                        //     fontSize: "12px", // Set font size here
-                                        //     fontWeight: "bold",
-                                        // }}
                                     >
                                         {statusData.map((entry, index) => (
                                             <Cell
@@ -286,6 +362,100 @@ export default function DashboardPage(options) {
                                 </PieChart>
                             </ResponsiveContainer>
                         </div>
+                    </div>
+                    <div className="my-10">
+                        <h1 className="text-center">Level Jabatan</h1>
+                        <ResponsiveContainer width="100%" height={200}>
+                            <BarChart
+                                data={dataJabatan}
+                                barSize={20}
+                                margin={{
+                                    top: 10,
+                                    right: 5,
+                                    left: 20,
+                                    bottom: 5,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="1 1" />
+                                <XAxis dataKey="status" />
+                                <YAxis />
+                                <Tooltip />
+                                <Bar dataKey="jumlah" fill="#805ed7">
+                                    <LabelList
+                                        dataKey="jumlah"
+                                        position="top"
+                                    />
+                                </Bar>
+                                <Legend
+                                    layout="vertical"
+                                    align="right"
+                                    // content={renderCustomLegend}
+                                    verticalAlign="middle"
+                                />
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
+                    <div className="my-10">
+                        <h1 className="text-center">Data Karyawan</h1>
+                        <ResponsiveContainer width="100%" height={200}>
+                            <BarChart
+                                data={dataDepartement}
+                                barSize={20}
+                                margin={{
+                                    top: 10,
+                                    right: 5,
+                                    left: 20,
+                                    bottom: 5,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="1 1" />
+                                <XAxis dataKey="status" />
+                                <YAxis />
+                                <Tooltip />
+                                <Bar dataKey="jumlah" fill="#805ed7">
+                                    <LabelList
+                                        dataKey="jumlah"
+                                        position="top"
+                                    />
+                                </Bar>
+                                <Legend
+                                    layout="vertical"
+                                    align="right"
+                                    // content={renderCustomLegend}
+                                    verticalAlign="middle"
+                                />
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
+                    <div>
+                        <h1 className="text-center">Report Training</h1>
+                        <ResponsiveContainer width="100%" height={200}>
+                            <LineChart
+                                data={reportTrainings}
+                                margin={{
+                                    top: 10,
+                                    right: 5,
+                                    left: 20,
+                                    bottom: 5,
+                                }}
+                            >
+                                <CartesianGrid />
+                                <XAxis dataKey="status" />
+                                <YAxis />
+                                <Tooltip />
+                                <Line
+                                    type="monotone"
+                                    dataKey="jumlah"
+                                    stroke="#8884d8"
+                                    fill="#8884d8"
+                                />
+                                <Legend
+                                    verticalAlign="middle"
+                                    align="right"
+                                    layout="vertical"
+                                />
+                            </LineChart>
+                        </ResponsiveContainer>
                     </div>
                 </div>
             </div>
